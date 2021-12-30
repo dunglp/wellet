cf69b8ff6bc3b300a77e29541a718c3a367a7e03
nhatanh <blackheron02@gmail.com>
2021-12-30 23:45:02 +0700

    Guaranteed reproducible build by pinning nixpkgs

--- shell.nix
+++ shell.nix
@@ -1,2 +1,9 @@
-with import <nixpkgs> { };
-stdenv.mkDerivation { name = "tronclone"; buildInputs = [ nodejs-14_x yarn python2Full ]; }
+{ pkgs ? import 
+  (fetchTarball 
+    https://releases.nixos.org/nixpkgs/nixpkgs-22.05pre340506.5c37ad87222/nixexprs.tar.xz) 
+    {} 
+}: with pkgs;
+  stdenv.mkDerivation 
+    { name = "tronclone";
+      buildInputs = [ nodejs-14_x yarn python2Full ];
+    }
