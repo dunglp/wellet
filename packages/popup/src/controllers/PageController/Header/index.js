d03e8177701770419acf1887facdc205b07128e8
nhatanh <blackheron02@gmail.com>
2022-01-15 01:05:40 +0700

    Updated header

--- packages/popup/src/controllers/PageController/Header/index.js
+++ packages/popup/src/controllers/PageController/Header/index.js
@@ -33,7 +33,7 @@ class Header extends React.Component {
             chains,
             handleShowChainList
         } = this.props;
-        const trxMarketUrl = developmentMode ? 'https://trx.market?from=tronlink' : 'https://trx.market?from=tronlink';
+        //const trxMarketUrl = developmentMode ? 'https://trx.market?from=tronlink' : 'https://trx.market?from=tronlink';
         return (
             <div className='header'>
                 <div className='titleContainer'>
@@ -52,25 +52,8 @@ class Header extends React.Component {
                         </div>
                     </div>
                     <div>
-                        <div className="linkWrap">
-                            {/*<a href="https://t.me/TronLink" target="_blank" data-tip='telegram' data-for='telegram' className="link link-telegram">&nbsp;</a>*/}
-                            {/*<ReactTooltip id='telegram' effect='solid' />*/}
-                            <a href="https://www.tronlink.org" target="_blank" data-tip={formatMessage({id:'INDEX_ICON_TITLE.OFFICIAL_WEBSITE'})} data-for="website" className="link link-home">&nbsp;</a>
-                            <ReactTooltip id='website' effect='solid' />
-                            <a href={trxMarketUrl} target="_blank" data-tip={formatMessage({id:'INDEX_ICON_TITLE.EXCHANGE'})} data-for="exchange" className="link link-exchange">&nbsp;</a>
-                            <ReactTooltip id='exchange' effect='solid' />
-                        </div>
-                        <div className="linkWrap">
-                            <a href="javascript:void(0)" data-tip={formatMessage({id:'CREATION.LEDGER.CONNECT_TITLE'})} data-for='ledger' onClick={()=>{
-                                PopupAPI.changeState(APP_STATE.LEDGER)
-                            }}  className="link link-ledger">&nbsp;</a>
-                            <ReactTooltip id='ledger' effect='solid' />
-                            <a href="javascript:void(0)" data-tip='Dapp' data-for='dapp' onClick={()=>{
-                                PopupAPI.setGaEvent('Dapp List','Recommend','Recommend')
-                                PopupAPI.changeState(APP_STATE.DAPP_LIST)
-                            }}  className="link link-dapp">&nbsp;</a>
-                            <ReactTooltip id='dapp' effect='solid' />
-                        </div>
+                        
+                        
                         <div>
                             <div className="fun" data-tip={formatMessage({id:'INDEX_ICON_TITLE.LOCK'})} data-for='lock' onClick={ () => { PopupAPI.lockWallet(); } }>&nbsp;</div>
                             <ReactTooltip id='lock' effect='solid' />
