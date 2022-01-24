3d687aa7bdf81654c141e338d9e8d5aaabe25e86
Dũng Lê <dunglp1303@gmail.com>
2022-01-24 12:29:02 +0700

    UI Tweaked P1

--- packages/popup/src/components/AccountName.js
+++ packages/popup/src/components/AccountName.js
@@ -1,160 +1,159 @@
-import React from 'react';
-import Input from '@tronlink/popup/src/components/Input';
-import Button from '@tronlink/popup/src/components/Button';
-import InputCriteria from '@tronlink/popup/src/components/InputCriteria';
+import React from "react";
+import Input from "@tronlink/popup/src/components/Input";
+import Button from "@tronlink/popup/src/components/Button";
+import InputCriteria from "@tronlink/popup/src/components/InputCriteria";
 
 // import { FormattedMessage } from 'react-intl';
-import { connect } from 'react-redux';
+import { connect } from "react-redux";
 
-import {
-    VALIDATION_STATE
-} from '@tronlink/lib/constants';
+import { VALIDATION_STATE } from "@tronlink/lib/constants";
 
 class AccountName extends React.Component {
-    state = {
-        name: '',
-        isValid: VALIDATION_STATE.NONE,
-        hasLength: false,
-        isAlphanumeric: false,
-        isUnique: false,
-        showCriteria: false
-    };
+	state = {
+		name: "",
+		isValid: VALIDATION_STATE.NONE,
+		hasLength: false,
+		isAlphanumeric: false,
+		isUnique: false,
+		showCriteria: false,
+	};
 
-    constructor() {
-        super();
+	constructor() {
+		super();
 
-        this.onChange = this.onChange.bind(this);
-    }
+		this.onChange = this.onChange.bind(this);
+	}
 
-    onChange(name) {
-        const { accounts } = this.props;
-        const trimmed = name.replace(/\s{2,}/g, ' ');
-        const showCriteria = trimmed.length===0?false:true;
-        const state = {
-            name: '',
-            isValid: VALIDATION_STATE.NONE,
-            hasLength: false,
-            isAlphanumeric: false,
-            isUnique: false,
-            showCriteria
-        };
+	onChange(name) {
+		const { accounts } = this.props;
+		const trimmed = name.replace(/\s{2,}/g, " ");
+		const showCriteria = trimmed.length === 0 ? false : true;
+		const state = {
+			name: "",
+			isValid: VALIDATION_STATE.NONE,
+			hasLength: false,
+			isAlphanumeric: false,
+			isUnique: false,
+			showCriteria,
+		};
 
-        if(/^\s$/.test(name) || !trimmed.length)
-            return this.setState(state);
+		if (/^\s$/.test(name) || !trimmed.length) return this.setState(state);
 
-        if(trimmed.trim().length >= 4)
-            state.hasLength = true;
+		if (trimmed.trim().length >= 4) state.hasLength = true;
 
-        if(/^([A-Za-z\d\s])+$/.test(trimmed))
-            state.isAlphanumeric = true;
+		if (/^([A-Za-z\d\s])+$/.test(trimmed)) state.isAlphanumeric = true;
 
-        if(!Object.values(accounts).some(({ name }) => name === trimmed.trim()))
-            state.isUnique = true;
+		if (
+			!Object.values(accounts).some(({ name }) => name === trimmed.trim())
+		)
+			state.isUnique = true;
 
-        if(state.hasLength && state.isAlphanumeric && state.isUnique)
-            state.isValid = VALIDATION_STATE.VALID;
-        else state.isValid = VALIDATION_STATE.INVALID;
+		if (state.hasLength && state.isAlphanumeric && state.isUnique)
+			state.isValid = VALIDATION_STATE.VALID;
+		else state.isValid = VALIDATION_STATE.INVALID;
 
-        state.name = trimmed;
-        this.setState(state);
-    }
+		state.name = trimmed;
+		this.setState(state);
+	}
 
-    render() {
-        const {
-            onCancel = () => {},
-            onSubmit = () => {}
-        } = this.props;
+	render() {
+		const { onCancel = () => {}, onSubmit = () => {} } = this.props;
 
-        const {
-            name,
-            isValid,
-            hasLength,
-            isAlphanumeric,
-            isUnique,
-            showCriteria
-        } = this.state;
+		const {
+			name,
+			isValid,
+			hasLength,
+			isAlphanumeric,
+			isUnique,
+			showCriteria,
+		} = this.state;
 
-        const isNameValid = isValid === VALIDATION_STATE.VALID;
+		const isNameValid = isValid === VALIDATION_STATE.VALID;
 
-        return (
-            <div className='insetContainer logoWrap'>
-                <div className="back" onClick={ onCancel }></div>
-                <div className='pageHeader'>
-                    <div className="pageHeaderLogoWrap hasBottomMargin">
-                        <div className="logo1"></div>
-                        <div className="logo2"></div>
-                    </div>
-                </div>
-                <div className='greyModal registrationModel'>
-                    <div className='inputGroup hasBottomMargin'>
-                        <Input
-                            className="accountName"
-                            placeholder='INPUT.ACCOUNT_NAME'
-                            status={ isValid }
-                            value={ name }
-                            onChange={ this.onChange }
-                            onEnter={ () => isNameValid && onSubmit(name) }
-                            tabIndex={ 1 }
-                        />
-                        {
-                            showCriteria?
-                                <div className='criteria'>
-                                    <InputCriteria id='CREATION_CRITERIA.HAS_LENGTH' isValid={ hasLength } />
-                                    <InputCriteria id='CREATION_CRITERIA.IS_ALPHANUMERIC' isValid={ isAlphanumeric } />
-                                    <InputCriteria id='CREATION_CRITERIA.IS_UNIQUE' isValid={ isUnique } />
-                                </div>
-                                :
-                                null
-                        }
-                    </div>
-                    <Button
-                        id='BUTTON.CONTINUE'
-                        isValid={ isNameValid }
-                        onClick={ () => isNameValid && onSubmit(name) }
-                        tabIndex={ 2 }
-                    />
-                </div>
-                {/*<div className='greyModal'>*/}
-                    {/*<div className='modalDesc hasBottomMargin'>*/}
-                        {/*<FormattedMessage id='ACCOUNT_NAME.DESC' />*/}
-                    {/*</div>*/}
-                    {/*<div className='inputGroup hasBottomMargin'>*/}
-                        {/*<Input*/}
-                            {/*icon='lock'*/}
-                            {/*placeholder='INPUT.ACCOUNT_NAME'*/}
-                            {/*status={ isValid }*/}
-                            {/*value={ name }*/}
-                            {/*onChange={ this.onChange }*/}
-                            {/*onEnter={ () => isNameValid && onSubmit(name) }*/}
-                            {/*tabIndex={ 1 }*/}
-                        {/*/>*/}
-                        {/*<div className='criteria'>*/}
-                            {/*<InputCriteria id='CREATION_CRITERIA.HAS_LENGTH' isValid={ hasLength } />*/}
-                            {/*<InputCriteria id='CREATION_CRITERIA.IS_ALPHANUMERIC' isValid={ isAlphanumeric } />*/}
-                            {/*<InputCriteria id='CREATION_CRITERIA.IS_UNIQUE' isValid={ isUnique } />*/}
-                        {/*</div>*/}
-                    {/*</div>*/}
+		return (
+			<div className="insetContainer logoWrap">
+				<div className="back" onClick={onCancel}></div>
+				<div className="pageHeader">
+					<div className="pageHeaderLogoWrap hasBottomMargin">
+						<div className="logo1"></div>
+					</div>
+				</div>
+				<div className="greyModal registrationModel">
+					<div className="inputGroup hasBottomMargin">
+						<Input
+							className="accountName"
+							placeholder="INPUT.ACCOUNT_NAME"
+							status={isValid}
+							value={name}
+							onChange={this.onChange}
+							onEnter={() => isNameValid && onSubmit(name)}
+							tabIndex={1}
+						/>
+						{showCriteria ? (
+							<div className="criteria">
+								<InputCriteria
+									id="CREATION_CRITERIA.HAS_LENGTH"
+									isValid={hasLength}
+								/>
+								<InputCriteria
+									id="CREATION_CRITERIA.IS_ALPHANUMERIC"
+									isValid={isAlphanumeric}
+								/>
+								<InputCriteria
+									id="CREATION_CRITERIA.IS_UNIQUE"
+									isValid={isUnique}
+								/>
+							</div>
+						) : null}
+					</div>
+					<Button
+						id="BUTTON.CONTINUE"
+						isValid={isNameValid}
+						onClick={() => isNameValid && onSubmit(name)}
+						tabIndex={2}
+					/>
+				</div>
+				{/*<div className='greyModal'>*/}
+				{/*<div className='modalDesc hasBottomMargin'>*/}
+				{/*<FormattedMessage id='ACCOUNT_NAME.DESC' />*/}
+				{/*</div>*/}
+				{/*<div className='inputGroup hasBottomMargin'>*/}
+				{/*<Input*/}
+				{/*icon='lock'*/}
+				{/*placeholder='INPUT.ACCOUNT_NAME'*/}
+				{/*status={ isValid }*/}
+				{/*value={ name }*/}
+				{/*onChange={ this.onChange }*/}
+				{/*onEnter={ () => isNameValid && onSubmit(name) }*/}
+				{/*tabIndex={ 1 }*/}
+				{/*/>*/}
+				{/*<div className='criteria'>*/}
+				{/*<InputCriteria id='CREATION_CRITERIA.HAS_LENGTH' isValid={ hasLength } />*/}
+				{/*<InputCriteria id='CREATION_CRITERIA.IS_ALPHANUMERIC' isValid={ isAlphanumeric } />*/}
+				{/*<InputCriteria id='CREATION_CRITERIA.IS_UNIQUE' isValid={ isUnique } />*/}
+				{/*</div>*/}
+				{/*</div>*/}
 
-                    {/*<div className='buttonRow'>*/}
-                        {/*<Button*/}
-                            {/*id='BUTTON.GO_BACK'*/}
-                            {/*type={ BUTTON_TYPE.DANGER }*/}
-                            {/*onClick={ onCancel }*/}
-                            {/*tabIndex={ 3 }*/}
-                        {/*/>*/}
-                        {/*<Button*/}
-                            {/*id='BUTTON.CONTINUE'*/}
-                            {/*isValid={ isNameValid }*/}
-                            {/*onClick={ () => isNameValid && onSubmit(name) }*/}
-                            {/*tabIndex={ 2 }*/}
-                        {/*/>*/}
-                    {/*</div>*/}
-                {/*</div>*/}
-            </div>
-        );
-    }
+				{/*<div className='buttonRow'>*/}
+				{/*<Button*/}
+				{/*id='BUTTON.GO_BACK'*/}
+				{/*type={ BUTTON_TYPE.DANGER }*/}
+				{/*onClick={ onCancel }*/}
+				{/*tabIndex={ 3 }*/}
+				{/*/>*/}
+				{/*<Button*/}
+				{/*id='BUTTON.CONTINUE'*/}
+				{/*isValid={ isNameValid }*/}
+				{/*onClick={ () => isNameValid && onSubmit(name) }*/}
+				{/*tabIndex={ 2 }*/}
+				{/*/>*/}
+				{/*</div>*/}
+				{/*</div>*/}
+			</div>
+		);
+	}
 }
 
-export default connect(state => ({
-    accounts: state.accounts.accounts
+export default connect((state) => ({
+	accounts: state.accounts.accounts,
 }))(AccountName);
--- packages/popup/src/controllers/LoginController.js
+++ packages/popup/src/controllers/LoginController.js
@@ -1,102 +1,105 @@
-import React from 'react';
-import Input from '@tronlink/popup/src/components/Input';
-import Button from '@tronlink/popup/src/components/Button';
+import React from "react";
+import Input from "@tronlink/popup/src/components/Input";
+import Button from "@tronlink/popup/src/components/Button";
 
-import { FormattedMessage } from 'react-intl';
-import { PopupAPI } from '@tronlink/lib/api';
-import { app } from '@tronlink/popup/src/index';
+import { FormattedMessage } from "react-intl";
+import { PopupAPI } from "@tronlink/lib/api";
+import { app } from "@tronlink/popup/src/index";
 
 class LoginController extends React.Component {
-    state = {
-        password: {
-            value: '',
-            isValid: false
-        },
-        loading: false,
-        error: false
-    };
+	state = {
+		password: {
+			value: "",
+			isValid: false,
+		},
+		loading: false,
+		error: false,
+	};
 
-    constructor() {
-        super();
+	constructor() {
+		super();
 
-        this.onPasswordChange = this.onPasswordChange.bind(this);
-        this.onButtonClick = this.onButtonClick.bind(this);
-    }
+		this.onPasswordChange = this.onPasswordChange.bind(this);
+		this.onButtonClick = this.onButtonClick.bind(this);
+	}
 
-    onPasswordChange(value) {
-        this.setState({
-            password: {
-                isValid: value.trim().length,
-                value
-            }
-        });
-    }
+	onPasswordChange(value) {
+		this.setState({
+			password: {
+				isValid: value.trim().length,
+				value,
+			},
+		});
+	}
 
-    onButtonClick() {
-        const { password } = this.state;
-        console.log(password);
-        this.setState({
-            loading: true
-        });
+	onButtonClick() {
+		const { password } = this.state;
+		console.log(password);
+		this.setState({
+			loading: true,
+		});
 
-        PopupAPI
-            .unlockWallet(password.value.trim())
-            .then(() => app.getAppState())
-            .catch(error => this.setState({
-                error
-            }))
-            .then(() => this.setState({
-                loading: false
-            }));
-    }
+		PopupAPI.unlockWallet(password.value.trim())
+			.then(() => app.getAppState())
+			.catch((error) =>
+				this.setState({
+					error,
+				})
+			)
+			.then(() =>
+				this.setState({
+					loading: false,
+				})
+			);
+	}
 
-    render() {
-        const {
-            password,
-            loading,
-            error
-        } = this.state;
+	render() {
+		const { password, loading, error } = this.state;
 
-        return (
-            <div className='insetContainer logoWrap'>
-                <div className='pageHeader'>
-                    <div className='pageHeaderLogoWrap'>
-                        <div className='logo1'></div>
-                        <div className='logo2'></div>
-                    </div>
-                    <div className='pageHeaderText'>
-                        <FormattedMessage id='LOGIN.PASSWORD.BACK' />
-                    </div>
-                </div>
-                { error ? (
-                    <div className='errorModal hasBottomMargin'>
-                        <FormattedMessage className='modalTitle' id='ERRORS.UNLOCK_FAILED' />
-                        <FormattedMessage className='modalBody' id={ error } />
-                    </div>
-                ) : '' }
-                <div className='greyModal loginModel'>
-                    <Input
-                        type='password'
-                        className='hasBottomMargin'
-                        placeholder='INPUT.PASSWORD'
-                        value={ password.value }
-                        isDisabled={ loading }
-                        onChange={ this.onPasswordChange }
-                        onEnter={ this.onButtonClick }
-                        tabIndex={ 1 }
-                    />
-            
-                    <Button
-                        id='BUTTON.CONTINUE'
-                        isValid={ password.isValid }
-                        isLoading={ loading }
-                        onClick={ this.onButtonClick }
-                        tabIndex={ 2 }
-                    />
-                </div>
-            </div>
-        );
-    }
+		return (
+			<div className="insetContainer logoWrap">
+				<div className="pageHeader">
+					<div className="pageHeaderLogoWrap">
+						<div className="logo1"></div>
+					</div>
+					<div className="pageHeaderText">
+						<FormattedMessage id="LOGIN.PASSWORD.BACK" />
+					</div>
+				</div>
+				{error ? (
+					<div className="errorModal hasBottomMargin">
+						<FormattedMessage
+							className="modalTitle"
+							id="ERRORS.UNLOCK_FAILED"
+						/>
+						<FormattedMessage className="modalBody" id={error} />
+					</div>
+				) : (
+					""
+				)}
+				<div className="greyModal loginModel">
+					<Input
+						type="password"
+						className="hasBottomMargin"
+						placeholder="123"
+						value={password.value}
+						isDisabled={loading}
+						onChange={this.onPasswordChange}
+						onEnter={this.onButtonClick}
+						tabIndex={1}
+					/>
+
+					<Button
+						id="BUTTON.CONTINUE"
+						isValid={password.isValid}
+						isLoading={loading}
+						onClick={this.onButtonClick}
+						tabIndex={2}
+					/>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default LoginController;
